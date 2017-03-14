77c4c80dd06ab500540cee58d00060067bda1b4c
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-03-14 16:17:38 -0700

    Added code to explain the class ClassInterfaceAttributeInfo.

--- UMLParser/src/com/amitakamat/umlparser/ClassInterfaceAttributeInfo.java
+++ UMLParser/src/com/amitakamat/umlparser/ClassInterfaceAttributeInfo.java
@@ -1,11 +1,18 @@
 package com.amitakamat.umlparser;
 
+/*
+ * Class to store the information of the atrributes of classes and interfaces
+ */
 public class ClassInterfaceAttributeInfo {
 		private String name; 
 		private String accessModifier;
 		private String dataType;
 		private String oneToOneRelationship;
 		private String oneToManyRelationship;
+		
+		/*
+		 * get set properties to get name of the attribute
+		 */
 		
 		public String getName(){
 			return this.name;
@@ -15,6 +22,9 @@ public class ClassInterfaceAttributeInfo {
 			this.name = name;
 		}
 		
+		/*
+		 * get set properties to get access modifier of the attribute
+		 */
 		public String getAccessModifier(){
 			return this.accessModifier;
 		}
@@ -22,6 +32,11 @@ public class ClassInterfaceAttributeInfo {
 		public void setAccessModifier(String accessModifier){
 			this.accessModifier = accessModifier;
 		}
+		
+		
+		/*
+		 * get set properties to get data type of the attribute
+		 */
 		
 		public String getDataType(){
 			return this.dataType;
@@ -31,6 +46,10 @@ public class ClassInterfaceAttributeInfo {
 			this.dataType = dataType;
 		}
 		
+		/*
+		 * get set properties to get one to one relationships of the attribute
+		 */
+		
 		public String getOneToOne(){
 			return this.oneToOneRelationship;
 		}
@@ -38,6 +57,10 @@ public class ClassInterfaceAttributeInfo {
 		public void setOneToOne(String oneToOne){
 			this.oneToOneRelationship = oneToOne;
 		}
+		
+		/*
+		 * get set properties to get one to many relationships of the attribute
+		 */
 		
 		public String getOneToMany(){
 			return this.oneToManyRelationship;
@@ -47,6 +70,9 @@ public class ClassInterfaceAttributeInfo {
 			this.oneToManyRelationship = oneToMany;
 		}
 		
+		/*
+		 * Parametrized constructor to create an object with the given values
+		 */
 		public ClassInterfaceAttributeInfo(String name, String accessModifier, String dataType, String oneToOne, String oneToMany){
 			this.name = name;
 			this.accessModifier = accessModifier;
