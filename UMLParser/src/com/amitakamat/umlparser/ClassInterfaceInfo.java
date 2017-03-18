22a8e25c6c8473988ba12ebef4c2b5995c28963e
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-03-17 23:28:52 -0700

    Added get properties in class ClassInterfaceInfo.

--- UMLParser/src/com/amitakamat/umlparser/ClassInterfaceInfo.java
+++ UMLParser/src/com/amitakamat/umlparser/ClassInterfaceInfo.java
@@ -8,6 +8,22 @@ public class ClassInterfaceInfo {
 	private ArrayList<String> implementsInfo;
 	private ArrayList<String> extendsInfo;
 	
+	public ArrayList<ClassInterfaceAttributeInfo> getAttributes(){
+		return this.attributes;
+	}
+	
+	public ArrayList<ClassInterfaceMethodInfo> getMethods(){
+		return this.methods;
+	}
+	
+	public ArrayList<String> getImplemensInfo(){
+		return this.implementsInfo;
+	}
+	
+	public ArrayList<String> getExtendsInfo(){
+		return this.extendsInfo;
+	}
+	
 	public ClassInterfaceInfo(ArrayList<ClassInterfaceAttributeInfo> attributes, ArrayList<ClassInterfaceMethodInfo> methods,
 							ArrayList<String> implementsInfo, ArrayList<String> extendsInfo) {
 		this.attributes = attributes;
