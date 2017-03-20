1d827e0c03c55c3d697b2b691115dc66f6ea9b9c
Amita Vasudev Kamat <amitavasudev.kamat@sjsu.edu>
2017-03-19 23:52:35 -0700

    Added documentation for class ClassInterfaceInfo.

--- UMLParser/src/com/amitakamat/umlparser/ClassInterfaceInfo.java
+++ UMLParser/src/com/amitakamat/umlparser/ClassInterfaceInfo.java
@@ -2,28 +2,49 @@ package com.amitakamat.umlparser;
 
 import java.util.ArrayList;
 
+/**
+ * 
+ * @author Amita Vasudev Kamat
+ * Class to store all the information of a class or an interface
+ *
+ */
 public class ClassInterfaceInfo {
 	private ArrayList<ClassInterfaceAttributeInfo> attributes;
 	private ArrayList<ClassInterfaceMethodInfo> methods;
 	private ArrayList<String> implementsInfo;
 	private ArrayList<String> extendsInfo;
 	
+	/*
+	 * Gets the attributes of a class or interface
+	 */
 	public ArrayList<ClassInterfaceAttributeInfo> getAttributes(){
 		return this.attributes;
 	}
 	
+	/*
+	 * Gets the methods of a class or an interface
+	 */
 	public ArrayList<ClassInterfaceMethodInfo> getMethods(){
 		return this.methods;
 	}
 	
+	/*
+	 * Gets the implements interface information of a class
+	 */
 	public ArrayList<String> getImplemensInfo(){
 		return this.implementsInfo;
 	}
 	
+	/*
+	 * Gets the extends class information of a class
+	 */
 	public ArrayList<String> getExtendsInfo(){
 		return this.extendsInfo;
 	}
 	
+	/*
+	 * Parameterized constructor for ClassInterfaceInfo class 
+	 */
 	public ClassInterfaceInfo(ArrayList<ClassInterfaceAttributeInfo> attributes, ArrayList<ClassInterfaceMethodInfo> methods,
 							ArrayList<String> implementsInfo, ArrayList<String> extendsInfo) {
 		this.attributes = attributes;
